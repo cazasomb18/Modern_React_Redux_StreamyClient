ebe8cd97bffe1a09d9d755df1e920022273a1727
claytonschoonoversmith@gmail.com <claytonschoonoversmith@gmail.com>
2021-01-14 02:23:06 -0600

    set up player in StreamShow.js, good example of when we need somethign to happen NO MATTER WHAT

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,21 +1,49 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import flv from 'flv.js';
 
 import { fetchStream } from '../../actions';
 
 class StreamShow extends React.Component {
-	componentDidMount(){
-		this.props.fetchStream(this.props.match.params.id);
+	constructor(props){
+		super(props);
+		this.videoRef = React.createRef();
 	}
+	componentDidMount(){
+		const { id } = this.props.match.params;
+
+		this.props.fetchStream(id);
+		this.buildPlayer();
+	}
+
+	componentDidUpdate(){
+		this.buildPlayer();
+	}
+
+	buildPlayer(){
+		if (this.player || !this.props.streams) {
+			return;
+		}
+
+		const { id } = this.props.match.params;
+		this.player = flv.createPlayer({
+			type: 'fly',
+			url: `http://localhost:8000/live/${id}.flv`
+		});
+		this.players.attachMediaElement(this.videoRef.current);
+		this.player.load();
+	}
+
 	render(){
 		if (!this.props.stream) {
-			return <div>Loading...</div>
+			return <div>Loading...</div>;
 		}
 
 		const { title, description } = this.props.stream;
 
 		return (
 			<div>
+				<video ref={this.videoRef} style={{width: '100%'}} controls/>
 				<h1>{title}</h1>
 				<h5>{description}</h5>
 			</div>
